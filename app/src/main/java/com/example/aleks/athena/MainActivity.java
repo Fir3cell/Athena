276fb05c03289c7682c896048f116d6dd88f54eb
mihrot <mihrot@gmail.com>
2015-05-17 02:17:07 +0200

    commit Miha 1

--- app/src/main/java/com/example/aleks/athena/MainActivity.java
+++ app/src/main/java/com/example/aleks/athena/MainActivity.java
@@ -2,16 +2,51 @@ package com.example.aleks.athena;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.widget.TextView;
+import java.security.KeyStore;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements LocationListener {
+
+    protected LocationManager locationManager;
+//    protected LocationListener locationListener;
+    protected Context context;
+    TextView txtLat;
+    String lat;
+    String provider;
+    protected String latitude,longitude;
+    protected boolean gps_enabled,network_enabled;
+
+//    protected LocationManager locationManager;
+    protected boolean LocationBool = true;
+    public Location location;
+//    LocationListener listener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtLat = (TextView) findViewById(R.id.textview1);
+
+        if (LocationBool)
+        {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 30000, 0,this);
+            }
+            else
+            {
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 30000, 0,this);
+            }
+        }
     }
 
     @Override
@@ -34,5 +69,68 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+//        txtLat = (TextView) findViewById(R.id.textview1);
+//        txtLat.setText("Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Log.d("Latitude","disable");
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Log.d("Latitude", "enable");
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        Log.d("Latitude","status");
+    }
+
+    public Location GetLocationInfo()
+    {
+        if (LocationBool)
+        {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 30000, 0,this);
+                if (locationManager != null)
+                {
+                    location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                }
+            }
+            else
+            {
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 30000, 0,this);
+                if (locationManager != null)
+                {
+                    location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                }
+            }
+        return location;
+        }
+        return null;
+    }
+
+    public void GetLocationOnClick(View x)
+    {
+        GetLocation();
+    }
+    public void GetLocation()
+    {
+        Log.d("Location", "Search started!");
+       // double longitude = location.getLongitude();
+       // double latitude = location.getLatitude();
+
+        GetLocationInfo();
+
+        txtLat = (TextView) findViewById(R.id.textview1);
+        txtLat.setText("Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
     }
 }
